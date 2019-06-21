507e1ad7f6a2e20649797c675da401be0c67c1b6
infinityname <52074475+infinityname@users.noreply.github.com>
2019-06-21 22:15:10 +0700

    Create icon_sprite.js

---
+++ icon_sprite.js
@@ -0,0 +1,6 @@
+            //:: Icon Spritemap
+            let icons = document.createElement('link');
+            icons.setAttribute('rel', 'stylesheet');
+            icons.type = 'text/css';
+            icons.href = '//cdn.vpthemes.win/light/icon_spritemap.css';
+            document.head.appendChild(icons);
