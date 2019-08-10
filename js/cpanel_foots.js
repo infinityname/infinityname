ec1282c82c87285ffcc395a7aa19683adf636927
infinityname <52074475+infinityname@users.noreply.github.com>
2019-08-10 13:10:14 +0700

    Update and rename cpanel_foot.js to cpanel_foots.js

--- js/cpanel_foots.js
+++ js/cpanel_foots.js
@@ -1,4 +1,3 @@
-
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -7,7 +6,6 @@ function getUrlVars() {
     return vars;
 }
 var fixtitle = getUrlVars()["option"];
-
 if (fixtitle === "gettingstarted") {
 document.title = 'cPanel - Getting Started';
 } else if (fixtitle === "pma") {
@@ -57,63 +55,43 @@ document.title = 'cPanel - SPF Records';
 }else if (fixtitle === "cloudflare") {
 document.title = 'cPanel - Cloudflare';
 }
-
-
 document.getElementById("lblLogout").innerText  = "LOGOUT";  
-
 <link rel="stylesheet" type="text/css" href="https://cdn.infinityname.host/assets/css/loader.css">
-<script src="https://cdn.infinityname.host/assets/js/loader.js">
-
+<script src="https://cdn.infinityname.host/assets/js/loader.js"></script>
 var st = '<div class="loader" id="loader"></div>',
 div = document.getElementById( 'wrap' );
 div.insertAdjacentHTML( 'beforeend', st );
-
-
 document.getElementById("lnkMenu").id = "sidebar_home";
 var st = '<a id="sidebar_user_manager" href="/panel/indexpl.php?option=accountsettings" title="" data-toggle="tooltip" data-placement="right" data-original-title="User Manager"><i class="fa fa-2x fa-group" id="imgUserManager"></i></a>',
 div = document.getElementById( 'sidebar_home' );
 div.insertAdjacentHTML( 'afterend', st );
-
 <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/infinityname/infinityname@master/icon_sprite.js">
-<script src="https://cdn.infinityname.host/assets/js/addon.js">
-
-<script src="https://githubusercontent.000webhostapp.com/cdn/cpanel-cd.js"> 
+</script>
+<script src="https://cdn.infinityname.host/assets/js/addon.js"></script>
+<script src="https://githubusercontent.000webhostapp.com/cdn/cpanel-cd.js"> </script>
 <!-- Begin Cookie Consent plugin by Silktide - https://silktide.com/cookieconsent -->
-<script type="text/javascript">
-    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"https://infinityname.host/privacy","theme":"dark-floating"};
-
+window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"https://infinityname.host/privacy","theme":"dark-floating"};
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js">
+</script>
 <!-- End Cookie Consent plugin -->
-
 document.addEventListener('DOMContentLoaded',function(){
-	element=document.querySelector('#lnkUserPrefChangeLang');element.parentElement.removeChild(element);
+element=document.querySelector('#lnkUserPrefChangeLang');element.parentElement.removeChild(element);
 });
-
-
 document.addEventListener('DOMContentLoaded',function(){
-	element=document.querySelector('#boxes');element.parentElement.removeChild(element);
+element=document.querySelector('#boxes');element.parentElement.removeChild(element);
 });
-
-
 document.addEventListener('DOMContentLoaded',function(){
-	element=document.querySelector('#lnkUserPrefChangeLang');element.parentElement.removeChild(element);
+element=document.querySelector('#lnkUserPrefChangeLang');element.parentElement.removeChild(element);
 });
-
-
 var index = 2;
 $('#sidebar').children().eq(index).remove();
-
-
 document.addEventListener('DOMContentLoaded',function(){
 	element=document.querySelector('#sidebar img');element.parentElement.removeChild(element);
 });
 document.addEventListener('DOMContentLoaded',function(){
 	element=document.querySelector('#sidebar href');element.parentElement.removeChild(element);
 });
-
-
  if(window.location.href.indexOf("accountsettings") > -1) {
        document.getElementById("sidebar_home").className = "";
        document.getElementById("sidebar_user_manager").className = "active";
     }
-
