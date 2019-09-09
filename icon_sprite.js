4b37b2101318d74b4ed0b28f41af688a84b21a11
infinityname <52074475+infinityname@users.noreply.github.com>
2019-09-09 20:00:57 +0700

    Update icon_sprite.js

--- icon_sprite.js
+++ icon_sprite.js
@@ -2,5 +2,5 @@
             let icons = document.createElement('link');
             icons.setAttribute('rel', 'stylesheet');
             icons.type = 'text/css';
-            icons.href = '//cdn.vpthemes.win/light/icon_spritemap.css';
+            icons.href = '//cdn.jsdelivr.net/gh/infinityname/infinityname@master/light_icon_spritemap.css';
             document.head.appendChild(icons);
