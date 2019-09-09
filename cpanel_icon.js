671202a7a7efe089dc136a1ddba789fea81b46f4
infinityname <52074475+infinityname@users.noreply.github.com>
2019-09-09 20:02:31 +0700

    Create cpanel_icon.js

---
+++ cpanel_icon.js
@@ -0,0 +1,6 @@
+            //:: Icon Spritemap
+            let icons = document.createElement('link');
+            icons.setAttribute('rel', 'stylesheet');
+            icons.type = 'text/css';
+            icons.href = '//cdn.jsdelivr.net/gh/infinityname/infinityname@master/light_icon_spritemap.css';
+            document.head.appendChild(icons);
